906b6e6343d56c9009781dbaaa151099498c0882
blara <bdlgrw@gmail.com>
2023-04-17 10:22:08 -0500

    [ADD] account_bioest

---
+++ account_bioest/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Account Bioest',
+    'summary': '''
+        Account Bioest
+    ''',
+    'description': '''
+        - Cuenta de gastos en categoría de producto permite seleccionar cuentas tipo gastos y otros.
+    ''',
+    'author': '1-800CLOUD',
+    'website': 'http://www.1-800cloud.com',
+    'category': 'Accounting/Accounting',
+    'license': 'LGPL-3',
+    'version': '15.0.0.0.1',
+    'depends': [
+        'account_baseline'
+    ],
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/product_category_views.xml',
+        # 'views/templates.xml',
+    ],
+}
