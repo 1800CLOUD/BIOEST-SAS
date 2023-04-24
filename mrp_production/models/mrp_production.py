ca8c6e435b01672b6bb54816149a1b6d135b3bf1
dfcastano <diegoferledesma@gmail.com>
2023-04-24 17:33:54 -0500

    [IMP] check availability method in mrp_production

--- mrp_production/models/mrp_production.py
+++ mrp_production/models/mrp_production.py
@@ -16,3 +16,11 @@ class MrpProduction(models.Model):
             else:
                 raise UserError(_('No hay suficiente cantidad disponible en la ubicación seleccionada.'))
         return True
+
+    def button_mark_done(self):
+        for production in self:
+            location_dest_id = production.move_raw_ids.location_id
+            product_qty_available = sum(location_dest_id.quant_ids.filtered(lambda q: q.product_id == production.product_id).mapped('quantity'))
+            if product_qty_available <= production.product_uom_qty:
+               raise UserError(_('No es posible marcar como hecho la orden de producción, dado que no hay suficiente cantidad disponible en la ubicación seleccionada.')) 
+        return super(MrpProduction, self).button_mark_done()
