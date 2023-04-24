ddeb9abd26bb29300e64277db2461bb6006c6338
dfcastano <diegoferledesma@gmail.com>
2023-04-24 17:00:30 -0500

    [IMP] check availability method in mrp_production

---
+++ mrp_production/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'mrp production',
+    'summary': '''
+        Production Bioest
+    ''',
+    'description': '''
+        - Cuenta de gastos en categoría de producto permite seleccionar cuentas tipo gastos y otros.
+    ''',
+    'author': '1-800CLOUD',
+    'website': 'http://www.1-800cloud.com',
+    'category': 'Accounting/Accounting',
+    'license': 'LGPL-3',
+    'version': '15.0.0.0.1',
+    'depends': [
+        'mrp'
+    ],
+    'data': [
+        # 'security/ir.model.access.csv',
+        #'views/product_category_views.xml',
+        # 'views/templates.xml',
+    ],
+}
---
+++ mrp_production/models/mrp_production.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, api, _
+from odoo.exceptions import UserError
+
+class MrpProduction(models.Model):
+    _inherit = 'mrp.production'
+
+    @api.multi
+    def action_assign(self):
+        for production in self:
+            location_dest_id = production.move_raw_ids.location_id
+            product_qty_available = sum(location_dest_id.quant_ids.filtered(lambda q: q.product_id == production.product_id).mapped('quantity'))
+            if product_qty_available >= production.product_uom_qty:
+                production.move_raw_ids._action_assign()
+            else:
+                raise UserError(_('No hay suficiente cantidad disponible en la ubicación seleccionada.'))
+        return True
