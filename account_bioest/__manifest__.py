128aeacb953aa51874440770e4b099d6ca07f442
dfcastano <diegoferledesma@gmail.com>
2023-05-29 17:15:41 -0500

    [IMP] Ir sequence, not validation sequence journals

--- account_bioest/__manifest__.py
+++ account_bioest/__manifest__.py
@@ -11,7 +11,7 @@
     'website': 'http://www.1-800cloud.com',
     'category': 'Accounting/Accounting',
     'license': 'LGPL-3',
-    'version': '15.0.0.0.2',
+    'version': '15.0.0.0.3',
     'depends': [
         'account_baseline'
     ],
